4e4a8732cb509429f1ecce3245cf4a681f077f5a
shivamkr1563 <145257370+shivamkr1563@users.noreply.github.com>
2025-05-05 18:18:39 +0000

    conatiner updates

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+ const accountId=21339
+ let accountEmail="shivamkr1562@gmail.com";
+ var accountPassword="123456789";    //bad pratice  pefer not to use var because issues in block scope and funtional scope
+ accountCity="Bihar";
+
+ accountEmail="vtu21339@veltech.edu.in";
+ accountPassword="22uecs";
+ accountCity="Delhi";
+
+ console.table([accountId,accountEmail,accountPassword,accountCity]);
+ 
