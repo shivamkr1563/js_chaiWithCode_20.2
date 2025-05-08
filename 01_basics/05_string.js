ebc3f038ce1c44871ddc6cf598a77360636cc78e
shivamkr1563 <145257370+shivamkr1563@users.noreply.github.com>
2025-05-08 13:15:54 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,39 @@
+const name ="shivam"
+const repoCount=50
+console.log(name +repoCount +" value");  //old method
+
+ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+ const gameName=new String('shivam-hero-com')
+ 
+ console.log(gameName[0]);
+ console.log(gameName.__proto__);
+
+ console.log(gameName.length);
+ console.log(gameName.toUpperCase());
+ console.log(gameName.charAt(2));
+ console.log(gameName.indexOf('i'));
+ 
+ const newString = gameName.substring(0,4);
+ console.log(newString);
+ const anotherString =gameName.slice(-16,4)
+ console.log(anotherString);
+
+ const newStringone="  shivam   "
+
+  console.log(newStringone);
+  console.log(newStringone.trim());
+
+  const url = "https:/shivam.com/shivam123kumar"
+  console.log(url.replace('123','-'));
+  console.log(url.includes('shivam'));
+  console.log(gameName.split('-'));
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
