ba980b8a9f2cfd5213fb50584c57a2e5297de4a7
shivamkr1563 <145257370+shivamkr1563@users.noreply.github.com>
2025-05-10 19:52:00 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,42 @@
+const arr = [1,2,3,4,5]
+const arr1 = new Array(1,2,3,4,5)
+const myHeros = ["shaktiman","naagraj"]
+//console.log(arr[0]);
+
+//Array methods
+
+// arr.push(6);
+// arr.pop();
+
+// arr.unshift(0)
+// arr.shift()
+// console.log(arr);
+
+// console.log(arr.includes(5));  // gives values are available in  array or not
+
+// console.log(arr.indexOf(5)); // print index of give  value from array
+
+
+//slice splice
+
+console.log("A", arr);
+
+const myarr1 = arr.slice(1,4);
+console.log(myarr1);
+
+console.log("B",arr);
+
+const myarr2 = arr.splice(1,4)
+console.log("c",arr);
+console.log(myarr2);
+
+
+
+
+
+
+
+
+
+
+
