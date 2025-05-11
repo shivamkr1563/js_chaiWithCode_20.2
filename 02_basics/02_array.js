365b3d80192effe730ffe1a0a5a1b497f14a8552
shivamkr1563 <145257370+shivamkr1563@users.noreply.github.com>
2025-05-11 09:53:31 +0000

    array methods

---
+++ 02_basics/02_array.js
@@ -0,0 +1,36 @@
+const marvel_heros = ["Ironman","spiderman","Thor"]
+const dc_heros = ["Batman","superman","Antman"]
+//push
+//marvel_heros.push(dc_heros)
+//console.log(marvel_heros);
+
+//concat
+// const All_heros =  marvel_heros.concat(dc_heros);
+// console.log(All_heros);
+
+//dot method
+// const All_hero = new Array(...marvel_heros,...dc_heros)
+// console.log(All_hero);
+
+// const newArray =[1,2,3,[5,6,7],8,[1,2,[4,5,6]]];
+
+// const Original_Array = newArray.flat(Infinity);
+// console.log(Original_Array);
+
+console.log(Array.isArray("shivam"));
+console.log(Array.from("shivam"));
+console.log(Array.from({name:"Amisha"}));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+let score4 = 400
+
+console.log(Array.of(score1,score2,score3,score4));
+
+
+
+
+
+
+
