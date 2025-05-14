e4b774355495821eb1b12defe735b3cf924bb919
shivamkr1563 <145257370+shivamkr1563@users.noreply.github.com>
2025-05-14 17:49:38 +0000

    functions

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -1,10 +1,37 @@
-function setMyName(){
-    console.log("s");
-    console.log("h");
-    console.log("i");
-    console.log("v");
-    console.log("a");
-    console.log("m");
-}
+// function setMyName(){
+//     console.log("s");
+//     console.log("h");
+//     console.log("i");
+//     console.log("v");
+//     console.log("a");
+//     console.log("m");
+// }
 
-setMyName();
+// setMyName();
+
+// function AddTwoNum(num1,num2){
+//     console.log(num1+num2);
+// }
+// AddTwoNum(3,5)
+
+
+// function AddTwoNum(num1,num2){
+//     // let result = num1+num2;
+//     // return result;
+//     return num1+num2;
+// }
+
+// let result = AddTwoNum(3,5);
+// console.log("result: ",result);
+
+// function loginUserMessage(username="sam"){
+//     if(username===undefined){
+//         console.log("Insert username");
+//         return;
+//     }
+//     return `${username} is loggedIn`;
+// }
+
+// let ans=loginUserMessage()
+// console.log(ans);
+
