c424c4296fe73d6cfcb6f92cac72557e813ff47d
shivamkr1563 <145257370+shivamkr1563@users.noreply.github.com>
2025-08-04 19:45:23 +0000

    iterative

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -1,29 +1,28 @@
-const coding = ["js","ruby","java","python","cpp"]
+// const coding = ["js", "ruby", "java", "python", "cpp"]
 
-const values =coding.forEach( (item) => {
-    //console.log(item);
-    return item
-    
-} )
-//console.log(values);
+
+// const values = coding.forEach( (item) => {
+//     //console.log(item);
+//     return item
+// } )
+
+// console.log(values);
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // const newNums = myNums.filter( (num) => {
-//     return num>4
+//     return num > 4
 // } )
 
-//console.log(newNums);
+// const newNums = []
 
+// myNums.forEach( (num) => {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// } )
 
-const newNums =[]
-
-myNums.forEach( (num) => {
-    if(num > 7){
-        newNums.push(num)
-    }
-})
-//console.log(newNums);
+// console.log(newNums);
 
 
 const books = [
@@ -38,17 +37,9 @@ const books = [
     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
   ];
 
+  let userBooks = books.filter( (bk) => bk.genre === 'History')
 
-//   let userBooks =books.filter( (bk) => bk.genre === 'History' )
-
-  //console.log(userBooks);
-  
-  var userBooks =books.filter( (bk) => {
-     return bk.publish >= 2000 && bk.genre === "History"
-  })
-
+  userBooks = books.filter( (bk) => { 
+    return bk.publish >= 1995 && bk.genre === "History"
+})
   console.log(userBooks);
-  
-
-
-
